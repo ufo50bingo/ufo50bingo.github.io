325f0b631a2b8c969970b742a6fd575ec4b5190b
Frank Thompson <frankthompson@gmail.com>
2025-04-28 16:49:14 -0700

    config

--- next.config.mjs
+++ next.config.mjs
@@ -12,4 +12,20 @@ export default withBundleAnalyzer({
   experimental: {
     optimizePackageImports: ['@mantine/core', '@mantine/hooks'],
   },
+    /**
+   * Enable static exports.
+   *
+   * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
+   */
+    output: "export",
+  
+    /**
+     * Disable server-based image optimization. Next.js does not support
+     * dynamic features with static exports.
+     *
+     * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
+     */
+    images: {
+      unoptimized: true,
+    },
 });

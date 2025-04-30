43fedcb779403c4db6a59dc3324f8be008b0b2dd
Frank Thompson <frankthompson@gmail.com>
2025-04-29 17:23:01 -0700

    update favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({ children }: { children: any }) {
     <html lang="en" {...mantineHtmlProps}>
       <head>
         <ColorSchemeScript />
-        <link rel="shortcut icon" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.png" />
         <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
